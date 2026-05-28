bbec9c209857e84d7ae586d5b7666d4f3691458b
VIKAS <vikasmukhiya1999@gmail.com>
2026-05-28 15:12:25 +0530

    fix: resolve robots.txt SEO paradox and make TagsList sticky

--- src/components/user/TagsList.jsx
+++ src/components/user/TagsList.jsx
@@ -14,6 +14,7 @@ export default async function TagsList() {
 
   return (
     <div className="
+      sticky top-16 md:top-[5.2rem] z-40
       min-h-16 border-b w-full bg-white dark:bg-zinc-900
       flex items-center gap-3 
       px-2 sm:px-12 md:px-16 lg:px-12 2xl:px-28
